e88bdee2bd8135b8f9f72a10185cb0381c7fecf9
Iwan <zach.limingxin@qq.com>
2023-07-23 17:06:42 +0800

    Original

---
+++ config/config.php
@@ -0,0 +1,15 @@
+<?php
+
+//site name
+define('SITE_NAME', 'yifengge.com');
+
+//App Root
+define('APP_ROOT', dirname(dirname(__FILE__)));
+define('URL_ROOT', '/');
+define('URL_SUBFOLDER', '');
+
+//DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '3459');
+define('DB_NAME', 'yifengge');
