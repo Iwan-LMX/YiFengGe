bf23a62d90413ee5e27644fcb06ddf1d06b5deac
Iwan <zach.limingxin@qq.com>
2023-08-02 18:55:40 +0800

    MVC Base

--- public/index.php
+++ public/index.php
@@ -1,5 +1,11 @@
 <?php
+// Autoloader
+require_once '../vendor/autoload.php';
+
 // Load Config
 require_once '../config/config.php';
 
-echo "hello world!";
+// Routes
+require_once '../routes/web.php';
+require_once '../app/Router.php';
+
